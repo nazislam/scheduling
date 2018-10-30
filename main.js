cce5557b755f922fb7fef22195c015a7c77a6df5
Naz Islam <naz@nazislam.com>
2018-10-30 07:49:13 +0000

    aa

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
+const Time = require('./time');
 
-const startTime = '07:00';
-const endTime = '22:00';
-let time = '';
-let a = [];
+const startTime = new Time(7, 0);
+const endTime = new Time(22, 0);
+let time = startTime;
+let a = []; // tasks without start time
+let b = []; // tasks with start time
+let runningTask;
 
 let doc = yaml.safeLoad(fs.readFileSync('./tasks.yml'));
 let tasks = doc['tasks'];
@@ -14,6 +17,8 @@ function prioritizeTask(taskList) {
   for (let i of taskList) {
     if (i.start === null) {
       a.push(i.duration);
+    } else {
+      b.push(i);
     }
   }
   let isSorted = false;
@@ -29,15 +34,41 @@ function prioritizeTask(taskList) {
     }
   }
   console.log(a);
+  console.log(b);
+}
+
+function getTaskByStart(startTime) {
+  for (task of tasks) {
+    if (startTime === task.start)
+      return task;
+  }
+}
+
+function printTask(task, startTime, endTime) {
+  console.log(task.description + ', ' + startTime + '-' + endTime);
 }
 
 function scheduleTask(taskArray) {
+  console.log(time.stringTime());
+  let f = time.stringTime();
+  runningTask = getTaskByStart(time.stringTime());
+  console.log(runningTask.description);
+  time.addMinute(runningTask.duration);
+  let t = time.stringTime();
+  console.log(time.stringTime());
+  printTask(runningTask, f, t);
 
+
+  /*
+  while (time !== endTime ) {
+    runningTask = getTaskByStart(time);
+    // printTask(runningTask, sTime, eTime);
+  }
+  */
 }
 
-
-// scheduleTask(tasks);
-prioritizeTask(tasks);
+scheduleTask(tasks);
+// prioritizeTask(tasks);
 
 
 
